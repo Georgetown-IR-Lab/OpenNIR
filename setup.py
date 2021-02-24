2cd72782870b27e8fe97fbea8d695ab6c405ed16
Cash Costello <cash.costello@jhuapl.edu>
2021-02-24 16:55:57 -0500

    exclude installing the tests as a module

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['test']),
     url="http://opennir.net/",
     project_urls={
         "Documentation": "http://opennir.net/",
